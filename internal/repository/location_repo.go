bae6468ed792ecaf4fc64442cb87b53bfe3b6cdd
unknown <sushant.shankar@vahanwire.com>
2025-12-10 16:29:31 +0530

    location code added

---
+++ internal/repository/location_repo.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"context"
+
+	"app_backend/internal/domain"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type LocationRepo struct {
+	col *mongo.Collection
+}
+
+func NewLocationRepo(db *mongo.Database) *LocationRepo {
+	return &LocationRepo{col: db.Collection("locations")}
+}
+
+// Save or update user location
+func (r *LocationRepo) Save(ctx context.Context, loc *domain.Location) error {
+	filter := bson.M{"userId": loc.UserID}
+	update := bson.M{"$set": loc}
+
+	_, err := r.col.UpdateOne(
+		ctx,
+		filter,
+		update,
+		options.Update().SetUpsert(true),
+	)
+	return err
+}
+
+// Get saved location by user
+func (r *LocationRepo) GetByUser(ctx context.Context, userID string) (*domain.Location, error) {
+	var loc domain.Location
+	err := r.col.FindOne(ctx, bson.M{"userId": userID}).Decode(&loc)
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	}
+	return &loc, err
+}
